588f56e074fd7ae8a02748e1be767154480686b0
Aid Khaoula <aidkhaoula977@gmail.com>
2025-06-27 19:19:39 +0200

    commit 3

--- config.py
+++ config.py
@@ -3,3 +3,10 @@ print (" on my way to learn python " *2 )
 khaoula = "pretty"
 print (khaoula)
 a =3
+message = '''
+a message to those who want to learn computer since
+     " you must have reselience "
+finghting
+'''
+print (message)
+new-line = "loose"
